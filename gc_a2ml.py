a57a32ff04b90afc215724c7117d71054c880725
Adam Blum <bluma@act.org>
2019-04-28 11:03:07 +0100

    turn into command line tool

--- gc_a2ml.py
+++ gc_a2ml.py
@@ -1,73 +1,113 @@
-project_id = 'automl-test-237311'
-compute_region = 'us-central1'
-dataset_id = 'moneyball'
-path = 'gs://moneyball/baseball.csv' 
-training_budget=3600
-
+import argparse 
+import os
 from google.cloud import automl_v1beta1 as automl
+# if A2ML with Google Cloud AutoML tables then
+# insure that your GOOGLE_APPLICATION_CREDENTIALS environment variable 
+# points to the application credentials file given from your Google Cloud SDK
+# you can also set a default PROJECT_ID for Google Cloud in your environment
+compute_region = 'us-central1'  # Google Cloud AutoML Tables is only 'us-central1' now
+training_budget=3600  # one hour of training time by default
 
-def model_callback(operation_future):
-    # Handle result.
-    result = operation_future.result()
-    return result
+def import_data(client,project_id,project_location,model_name,source): 
 
-client = automl.AutoMlClient()
-project_location = client.location_path(project_id,"us-central1")
-print('Location:{}'.format(project_location))
+    create_dataset_response = client.create_dataset(
+        project_location,
+        {'display_name': model_name,
+        'tables_dataset_metadata': {}})
+    dataset_id = create_dataset_response.name
+    dataset_full_id = client.dataset_path(project_id, compute_region, dataset_id)
+    print('Created dataset: {}'.format(dataset_id))
+    print('Full ID: {}'.format(dataset_full_id))
 
-create_dataset_response = client.create_dataset(
-    project_location,
-    {'display_name': dataset_id,
-    'tables_dataset_metadata': {}})
-dataset_name = create_dataset_response.name
-dataset_id = dataset_name.split("/"[-1])
-dataset_full_id = client.dataset_path(
-    project_id, compute_region, dataset_id)
+    if source.startswith('bq'):
+        input_config = {"bigquery_source": {"input_uri": source}}
+    else:
+        # Get the multiple Google Cloud Storage URIs.
+        input_uris = source.split(",")
+        input_config = {"gcs_source": {"input_uris": input_uris}}
 
-if path.startswith('bq'):
-    input_config = {"bigquery_source": {"input_uri": path}}
-else:
-    # Get the multiple Google Cloud Storage URIs.
-    input_uris = path.split(",")
-    input_config = {"gcs_source": {"input_uris": input_uris}}
+    print("Processing import: {}".format(dataset_id))
+    operation= client.import_data(dataset_id, input_config)
+    # synchronous check of operation status.
+    import_data_response = operation.result()
+    print("Imported data: {}".format(import_data_response))
+    return dataset_id
 
-print("Processing import...")
-response = client.import_data(dataset_full_id, input_config)
+def train_model(client,project_id,project_location,dataset_name,target,model_name,training_budget):
+    list_table_specs_response= client.list_table_specs(dataset_name)
 
-# synchronous check of operation status.
-print("Dataimported")
+    table_specs = [s for s in list_table_specs_response]
 
+    table_spec_name = table_specs[0].name
+    list_column_specs_response = client.list_column_specs(table_spec_name)
+    column_specs = {s.display_name: s for s in list_column_specs_response}
 
-list_table_specs_response = client.list_table_specs(dataset_name)
-table_specs = [s for s in list_table_specs_response]
+    label_column_name = target
+    label_column_spec = column_specs[label_column_name]
+    label_column_id = label_column_spec.name.rsplit('/', 1)[-1]
+    update_dataset_dict = {
+        'name': dataset_name, 
+        'tables_dataset_metadata': {'target_column_spec_id': label_column_id}}
+    update_dataset_response = client.update_dataset(update_dataset_dict)
+    print("Updated dataset response: {}".format(update_dataset_response))
+    feat_list = list(column_specs.keys())
+    feat_list.remove(target)
+    
+    model_dict = {
+    'display_name': model_name,
+    'dataset_id': dataset_name.rsplit('/', 1)[-1],
+    'tables_model_metadata': {
+        'target_column_spec': column_specs[target],
+        'input_feature_column_specs': [
+            column_specs[x] for x in feat_list],
+        'train_budget_milli_node_hours': training_budget,
+        'optimization_objective': 'MINIMIZE_MAE'}}
 
-table_spec_name = table_specs[0].name
-list_column_specs_response = client.list_column_specs(table_spec_name)
-column_specs = {s.display_name: s for s in list_column_specs_response}
-TARGET_LABEL = 'target_monetary'
-label_column_name = TARGET_LABEL
-label_column_spec = column_specs[label_column_name]
-label_column_id = label_column_spec.name.rsplit('/', 1)[-1]
-update_dataset_dict = {
-    'name': dataset_name, 
-    'tables_dataset_metadata': {'target_column_spec_id': label_column_id}}
+    print(:"Training model: {}".format(model_name))
+    operation = client.create_model(project_location, model_dict)
+    model_response = operation.result()
 
-update_dataset_response = client.update_dataset(update_dataset_dict)
-model_display_name = dataset_id 
-model_training_budget = training_budget * 1000
-model_dict = {
-  'display_name': model_display_name,
-  'dataset_id': dataset_name.rsplit('/',1)[-1],
-  'tables_model_metadata': {
-      'target_column_spec': column_specs['target_monetary'],
-      'input_feature_column_specs': [
-          column_specs[x] for x in feat_list],
-      'train_budget_milli_node_hours': model_training_budget,
-      'optimization_objective': 'MINIMIZE_MAE'}}
+    # Handle metadata.
+    metadata = model_response.metadata()
+    print("Model creation response: {}".format(metadata))
+    return metadata
 
-model = {}
-model_response = client.create_model(project_location, model)
-model_response.add_done_callback(model_callback)
+def main():
 
-# Handle metadata.
-metadata = model_response.metadata()
+    client = automl.AutoMlClient()
+
+    parser = argparse.ArgumentParser(description='Automate AutoML Pipeline.')
+    parser.add_argument('-p','--project',help='Google Cloud project ID')
+    parser.add_argument('-d','--dataset',help='Google Cloud dataset ID')
+    parser.add_argument('-m','--model',help='Model name')
+    parser.add_argument('-s','--source',help='Source path for loading dataset')
+    parser.add_argument('-t','--target',help='Target column from dataset')
+    parser.add_argument('-b','--budget',type=int, help='Training time in seconds')
+    parser.add_argument('-i','--import_first',help='Perform import before training')
+    args = parser.parse_args()
+    if (args.project is not None):
+        project_id = args.project
+    else:  # default project read from environment
+        project_id = os.getenv('PROJECT_ID')
+
+    if (args.dataset is not None):
+        dataset_id = args.dataset 
+        dataset_name = client.dataset_path(project_id, compute_region, dataset_id)
+    if (args.target is not None):
+        target = args.target
+    if (args.model is not None):
+        model_name = args.model
+    if (args.source is not None):
+        source = args.source 
+    if (args.budget is not None):
+        training_budget = args.budget
+    else:
+        training_budget = 3600
+    
+    project_location = client.location_path(project_id,"us-central1")
+    if (args.import_first): 
+        dataset_name=import_data(client,project_id, project_location,model_name,source)
+    train_model(client,project_id,project_location,dataset_name,target,model_name,training_budget)
+
+if __name__ == '__main__':
+    main()
