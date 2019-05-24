2bc95b3ec73b846afc472dab1f01ba899a5ce62e
vlad-dplrn <vlad@dplrn.com>
2019-05-23 17:48:29 -0700

    added wait for post uplohad processing

--- a2ml/api/auger/hub/project_file.py
+++ a2ml/api/auger/hub/project_file.py
@@ -13,6 +13,17 @@ class AugerProjectFileApi(object):
         return self.hub_client.request_list('project_files', params)
 
     def create(self, data_source_name, filename, file_url):
-        return self.hub_client.call_hub_api('create_project_file',
+        project_file = self.hub_client.call_hub_api('create_project_file',
             { 'name': data_source_name, 'project_id': self.project_id,
               'file_name': filename, 'url': file_url })
+
+        if 'id' in project_file:
+            project_file = self.hub_client.wait_for_object_status(
+                method='get_project_file',
+                params={'id': project_file['id']},
+                status=project_file['status'],
+                progress=[
+                    'processing'
+                ])
+                
+        return project_file
