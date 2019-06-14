12db7b8344a60a285a83ae3b14bc7fdd3bc8af05
vlad-dplrn <vlad@dplrn.com>
2019-06-14 11:58:43 +0300

    split requirements back into groups

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@ from setuptools import setup
 from setuptools import find_packages
 
 install_requires = [
-    'click', 'scikit-learn<=0.20.3,>=0.19.0', 'sklearn', 'wheel==0.30.0',
-    'requests', 'requests-toolbelt', 'shortuuid', 'pandas<=0.23.4,>=0.21.0',
-    'lightgbm<=2.2.1,>=2.0.11', 'scipy<=1.1.0,>=1.0.0', 'numpy<=1.16.2,>=1.11.0',
-    'ruamel.yaml<=0.15.89,>=0.15.35', 'auger-hub-api-client>=0.5.6',
-    'azureml', 'azureml.core', 'azureml.train', 'azureml.train.automl',
-    'google-cloud-automl']
+    'click','scikit-learn<=0.20.3,>=0.19.0','sklearn','wheel==0.30.0',
+    'requests','requests-toolbelt','shortuuid','ruamel.yaml<=0.15.89,>=0.15.35',
+    'pandas<=0.23.4,>=0.21.0','auger-hub-api-client>=0.5.6'
+]
 
 extras = {
-    'testing': ['pytest', 'pytest-cov', 'pytest-xdist', 'flake8', 'mock']
+    'testing': ['pytest', 'pytest-cov', 'pytest-xdist', 'flake8', 'mock'],
+    'azure': ['lightgbm<=2.2.1,>=2.0.11','scipy<=1.1.0,>=1.0.0',
+        'numpy<=1.16.2,>=1.11.0','azureml','azureml.core','azureml.train',
+        'azureml.train.automl'],
+    'google': ['google-cloud-automl']
 }
 
 # Meta dependency groups.
@@ -22,8 +24,8 @@ extras['all'] = all_deps
 setup(
     name='a2ml',
     version='0.1',
-    description=('The A2ML ("Automate AutoML") project is a set of scripts to automate '
-                 'Automated Machine Learning tools from multiple vendors.'),
+    description=('The A2ML ("Automate AutoML") project is a set of scripts to '
+                 'automate Automated Machine Learning tools from multiple vendors.'),
     author='Deep Learn',
     author_email='augerai@dplrn.com',
     url='https://github.com/deeplearninc/a2ml',
