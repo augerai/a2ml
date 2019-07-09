c61baf1c8542000c747920468ea3e18da763d37b
vlad-dplrn <vlad@dplrn.com>
2019-07-09 11:55:27 +0300

    minor cleanup

--- a2ml/cmdl/commands/cmd_deploy.py
+++ a2ml/cmdl/commands/cmd_deploy.py
@@ -2,19 +2,6 @@ import click
 from a2ml.api.a2ml import A2ML
 from a2ml.api.utils.context import pass_context
 
-class DeployCmd(object):
-
-    def __init__(self, ctx):
-        self.ctx = ctx
-
-    def deploy(self, model_id, locally):
-        operations = {
-            'auger': AugerDeploy(self.ctx.copy('auger')).deploy,
-            'google': GoogleDeploy(self.ctx.copy('google')).deploy
-        }
-        ProviderOperations(self.ctx).execute(
-            self.ctx.get_providers(), operations,
-            model_id=model_id, locally=locally)
 
 @click.command('deploy', short_help='Deploy trained model.')
 @click.argument('model-id', required=True, type=click.STRING)
