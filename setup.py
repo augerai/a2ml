7fd77eeccb7173351a77dfba5f653bba5597e088
Evgeny Vovchenko <evgeny.vovchenko@act.org>
2019-10-18 12:21:58 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ class VerifyVersionCommand(install):
             sys.exit(info)
 
 install_requires = [
-    'click','scikit-learn<=0.20.3,>=0.19.0','sklearn','wheel==0.30.0',
+    'click','scikit-learn>=0.19.0','wheel==0.30.0',
     'requests','requests-toolbelt','shortuuid','ruamel.yaml<=0.15.89,>=0.15.35',
     'pandas<=0.23.4,>=0.21.0','auger-hub-api-client>=0.6.1', 'auger.ai'
 ]
