bf2bc1f246819b0a0b1ef85e583a25b566a310af
vlad-dplrn <vlad@dplrn.com>
2020-03-03 17:26:26 -0800

    return results from import; refactored auger and azure import op

--- a2ml/api/auger/import_data.py
+++ a2ml/api/auger/import_data.py
@@ -1,42 +1,27 @@
-from a2ml.api.auger.base import AugerBase
+from auger.api.dataset import DataSet
+from auger.cli.utils.decorators import \
+    error_handler, authenticated, with_project
+from auger.api.cloud.rest_api import RestApi
+from auger.api.credentials import Credentials
 from a2ml.api.auger.config import AugerConfig
-from auger.api.cloud.data_set import AugerDataSetApi
 
 
-class AugerImport(AugerBase):
+class AugerImport(object):
     """Import data into Auger."""
 
     def __init__(self, ctx):
-        super(AugerImport, self).__init__(ctx)
+        super(AugerImport, self).__init__()
+        self.ctx = ctx
+        self.ctx.credentials = Credentials(ctx).load()
+        self.ctx.rest_api = RestApi(
+            self.ctx.credentials.api_url, self.ctx.credentials.token)
 
-    @AugerBase._error_handler
-    def import_data(self):
-        # verify avalability of auger credentials
-        self.credentials.verify()
-
-        # verify there is a source file for importing
-        file_to_upload = self._get_source_file()
-
-        self.ctx.log('Importing file %s' % file_to_upload)
-
-        self.start_project()
-
-        data_set_api = AugerDataSetApi(self.ctx, self.project_api)
-        data_set_api.create(file_to_upload)
-        AugerConfig(self.ctx).set_data_set(data_set_api.object_name)
-
-        self.ctx.log(
-            'Created DataSet %s on Auger Cloud.' % \
-             data_set_api.object_name)
-        self.ctx.log(
-            'DataSet name stored in auger.yaml/dataset')
-
-    def _get_source_file(self):
-        file_to_upload = self.ctx.config.get('source', None)
-
-        if file_to_upload is None:
-            raise Exception(
-                'Please specify source in config.yaml '
-                'to import to Auger Cloud...')
-
-        return AugerDataSetApi.verify(file_to_upload)[0]
+    @error_handler
+    @authenticated
+    @with_project(autocreate=True)
+    def import_data(self, project):
+        source = self.ctx.config.get('source', None)
+        dataset = DataSet(self.ctx, project).create(source)
+        AugerConfig(self.ctx).set_data_set(dataset.name)
+        self.ctx.log('Created DataSet %s' % dataset.name)
+        return {'dataset': dataset.name}
