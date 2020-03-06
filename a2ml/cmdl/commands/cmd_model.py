ac9c026c8ee4f7219e52949446ba4804da77f829
derekflint <30329594+derekflint@users.noreply.github.com>
2020-03-06 13:38:01 -0800

    add acutal

--- a2ml/cmdl/commands/cmd_model.py
+++ a2ml/cmdl/commands/cmd_model.py
@@ -35,11 +35,23 @@ def predict(ctx, provider, filename, model_id, threshold, locally):
     """Predict with deployed model."""
     A2MLModel(ctx, provider).predict(filename, model_id, threshold, locally)
 
+@click.command('actual', short_help='Send actual values for deployed model. Needed for review and monitoring.')
+@click.argument('filename', required=True, type=click.STRING)
+@click.option('--model-id', '-m', type=click.STRING, required=True,
+    help='Deployed model id.')
+@click.option('--provider', '-p', type=click.STRING, required=False,
+help='Cloud AutoML Provider.')
+@pass_context
+def actual(ctx, provider, filename, model_id):
+    """Predict with deployed model."""
+    A2MLModel(ctx, provider).actual(filename, model_id)
+
 
 @pass_context
 def add_commands(ctx):
     cmdl.add_command(deploy)
     cmdl.add_command(predict)
+    cmdl.add_command(actual)
 
 
 add_commands()
