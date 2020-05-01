4068bda0f888fafd10ebacd50cd83d8c7df633c7
skatedplrn <evgeny@dplrn.com>
2020-05-01 08:30:15 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 from setuptools import find_packages
 from setuptools.command.install import install
 
-VERSION = '0.3.4'
+VERSION = '0.4.0.dev0'
 
 # Get the long description from the README file
 here = os.path.abspath(os.path.dirname(__file__))
