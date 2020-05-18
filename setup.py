a09aed50418f9a773e4ed503e740700575d06cee
skatedplrn <evgeny@dplrn.com>
2020-05-18 19:12:33 +0300

    0.3.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 from setuptools import find_packages
 from setuptools.command.install import install
 
-VERSION = '0.3.8'
+VERSION = '0.3.9'
 
 # Get the long description from the README file
 here = os.path.abspath(os.path.dirname(__file__))
