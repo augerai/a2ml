adb105385e30c0f0aa9d1d9b3f242e15d8beec80
Alex Emelyanov <holyketzer@gmail.com>
2020-06-08 16:31:55 +0300

    Remove spinner

--- a2ml/api/utils/provider_runner.py
+++ a2ml/api/utils/provider_runner.py
@@ -46,45 +46,27 @@ class ProviderRunner(object):
                 'at config.yaml/providers')
         # if there is single operation requested
         # no need to run it on the thread
-        with self.with_spinning_cursor():
-            if len(self.providers) == 1:
-                provider_name = list(self.providers.keys())[0]
-                return self.execute_provider(provider_name, operation_name, *args, **kwargs)
+        if len(self.providers) == 1:
+            provider_name = list(self.providers.keys())[0]
+            return self.execute_provider(provider_name, operation_name, *args, **kwargs)
 
-            with ThreadPoolExecutor(max_workers=len(self.providers)) as executor:
-                futures = {
-                    executor.submit(
-                        self.execute_provider, provider_name, operation_name, *args, **kwargs): provider_name
-                    for provider_name in self.providers.keys()
-                }
+        with ThreadPoolExecutor(max_workers=len(self.providers)) as executor:
+            futures = {
+                executor.submit(
+                    self.execute_provider, provider_name, operation_name, *args, **kwargs): provider_name
+                for provider_name in self.providers.keys()
+            }
 
-                try:
-                    results = {}
-                    for future in as_completed(futures):
-                        results.update(future.result())
-                    return results
-                except KeyboardInterrupt:
-                    # not a graceful termination
-                    executor._threads.clear()
-                    thread._threads_queues.clear()
-                    raise
-
-    @contextlib.contextmanager
-    def with_spinning_cursor(self):
-        current_process = psutil.Process(os.getpid())
-        config = self.ctx.config
-
-        # Do not show spinner on server side, on client side if it's running inside Celery process
-        # or if it's explicitly disabled in the config
-        if config.get('use_server') and current_process.name() != 'celery' and not config.get('no_spinner'):
             try:
-                spinning_cursor = self.SpinningCursorThread()
-                spinning_cursor.start()
-                yield
-            finally:
-                spinning_cursor.stop()
-        else:
-            yield
+                results = {}
+                for future in as_completed(futures):
+                    results.update(future.result())
+                return results
+            except KeyboardInterrupt:
+                # not a graceful termination
+                executor._threads.clear()
+                thread._threads_queues.clear()
+                raise
 
     def execute_provider(self, provider_name, operation_name, *args, **kwargs):
         try:
