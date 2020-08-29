af34c8d3bcab3f8d9b19770b3d7935824ccdba56
skatedplrn <evgeny@dplrn.com>
2020-08-29 18:20:24 +0300

    use calc_score flag in score_actuals_by_model_task

--- a2ml/tasks_queue/tasks_hub_api.py
+++ a2ml/tasks_queue/tasks_hub_api.py
@@ -501,6 +501,7 @@ def score_actuals_by_model_task(params):
             params.get('hub_info', {}).get('project_path')),
         actual_date=params.get('actual_date'),
         actuals_id=params.get('actuals_id'),
+        calc_score=params.get('calc_score', False),
         return_count=params.get('return_count', False)
     )
 
