cfa5d90b9fdac7bd2840a537990a6c211e812f42
skatedplrn <evgeny@dplrn.com>
2020-11-12 11:45:31 +0300

    update regression scores

--- a2ml/api/model_review/scores/regression.py
+++ a2ml/api/model_review/scores/regression.py
@@ -3,6 +3,27 @@ from sklearn.metrics import make_scorer, mean_squared_error, mean_squared_log_er
 from sklearn.metrics.scorer import SCORERS
 
 
+EPSILON = 1e-10
+
+def _error(actual: np.ndarray, predicted: np.ndarray):
+    """ Simple error """
+    return actual - predicted
+
+def _percentage_error(actual: np.ndarray, predicted: np.ndarray):
+    """
+    Percentage error
+    Note: result is NOT multiplied by 100
+    """
+    return _error(actual, predicted) / (actual + EPSILON)
+
+def mae(actual: np.ndarray, predicted: np.ndarray):
+    """ Mean Absolute Error """
+    return np.mean(np.abs(_error(actual, predicted)))
+
+def _naive_forecasting(actual: np.ndarray, seasonality: int = 1):
+    """ Naive forecasting method which just repeats previous samples """
+    return actual[:-seasonality]
+
 def neg_rmsle_score(y_true, y_pred):
     return -np.sqrt(mean_squared_log_error(y_true, y_pred))
 
@@ -10,44 +31,36 @@ def neg_rmsle_score(y_true, y_pred):
 def neg_rmse_score(y_true, y_pred):
     return -np.sqrt(mean_squared_error(y_true, y_pred))
 
-
-def neg_mase_score(y_true, y_pred):
-    y_true = np.asarray(y_true)
-    y_pred = np.asarray(y_pred)
-    numerator = np.sum(np.abs(y_true - y_pred))
-    coeff = y_true.shape[0] / (y_true.shape[0] - 1)
-    denominator = np.sum(np.abs(y_true[1:] - y_true[:-1]))
-    if np.abs(denominator) < 1e-12:     # straight line
-        denominator = 1.0
-    return -numerator / (denominator * coeff)
-
+def neg_mase_score(actual: np.ndarray, predicted: np.ndarray, seasonality: int = 1):
+    """
+    Mean Absolute Scaled Error
+    Baseline (benchmark) is computed with naive forecasting (shifted by @seasonality)
+    """
+    return -mae(actual, predicted) / mae(actual[seasonality:], _naive_forecasting(actual, seasonality))
 
 def neg_mape_score(y_true, y_pred):
-    eps = 1e-8
-    y_true = np.asarray(y_true)
-    y_pred = np.asarray(y_pred)
-    result = -np.sum(np.abs((y_true - y_pred) / (y_true + eps) )) / len(y_true)
-    return float(result)
+    """
+    Mean Absolute Percentage Error
+    Properties:
+        + Easy to interpret
+        + Scale independent
+        - Biased, not symmetric
+        - Undefined when actual[t] == 0
+    Note: result is NOT multiplied by 100
+    """
+    #return -np.mean(np.abs(_percentage_error(y_true, y_pred)))
 
+    #smdape Symmetric Median Absolute Percentage Error
+    #return -np.mean(2.0 * np.abs(y_true - y_pred) / ((np.abs(y_true) + np.abs(y_pred)) + EPSILON))
+    
+    #maape - Mean Arctangent Absolute Percentage Error
+    return -np.mean(np.arctan(np.abs((y_true - y_pred) / (y_true + EPSILON))))
 
-def mda_score(y_true, y_pred, above_percent=0.2):
-    """ https://en.wikipedia.org/wiki/Mean_Directional_Accuracy """
-    y_true = np.asarray(y_true)
-    y_pred = np.asarray(y_pred)
-
-    y_diff = y_true[1:] - y_true[:-1]
-    y_diff_mean = np.mean(y_diff)
-
-    idx = np.where(y_diff / y_diff_mean > above_percent)[0]
-    true_sign = np.sign(y_diff[idx])
-
-    y_diff = y_pred[1:] - y_true[:-1]
-    idx_eliminate = np.where(y_diff / y_diff_mean > above_percent)[0]
-    y_diff[idx_eliminate] = 0.
-    pred_sign = np.sign(y_diff[idx])
-
-    match = np.sum(true_sign == pred_sign)
-    return match / float(idx.shape[0])
+# https://en.wikipedia.org/wiki/Mean_Directional_Accuracy
+#https://gist.github.com/bshishov/5dc237f59f019b26145648e2124ca1c9
+def mda_score(y_true, y_pred):
+    """ Mean Directional Accuracy """
+    return np.mean((np.sign(y_true[1:] - y_true[:-1]) == np.sign(y_pred[1:] - y_pred[:-1])).astype(int))
 
 def nmae_score(y_true, y_pred):
     """ Normalized Root Mean Squared Error """
