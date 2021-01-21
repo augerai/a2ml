426e05273cd289ff2dcedf37ae1a8ea7fe7028cc
Alex Emelyanov <holyketzer@gmail.com>
2021-01-21 18:25:30 +0300

    Make node evaluation multiline + Support agg functions

--- a2ml/api/utils/roi_calc.py
+++ a2ml/api/utils/roi_calc.py
@@ -1,3 +1,5 @@
+import json
+
 PLUS = "+"
 MINUS = "-"
 MULTIPLICATION = "*"
@@ -7,11 +9,21 @@ OPENING_BRACKET = "("
 CLOSING_BRACKET = ")"
 
 COMMA = ","
+AT = "@"
+DOLLAR = "$"
+
+LT = "<"
+EQ = "="
+GT = ">"
+EXCLAMATION = "!"
 
 OPERATORS = set([PLUS, MINUS, MULTIPLICATION, DIVISION])
 BRACKETS = set([OPENING_BRACKET, CLOSING_BRACKET])
-SYMBOLS = set([COMMA, "@"])
+COMPARISON_SYMBOLS = set([LT, EQ, GT, EXCLAMATION])
+SYMBOLS = set([COMMA])
 WHITESPACES = set([" ", "\t", "\n", "\r"])
+
+COMPARISON_OPS = set(["<", ">", "<=", ">=", "=", "!="])
 
 def is_digit(c):
     return c >= '0' and c <= '9'
@@ -19,11 +31,14 @@ def is_digit(c):
 def is_opearator(c):
     return c in OPERATORS
 
+def is_comparison_sym(c):
+    return c in COMPARISON_SYMBOLS
+
 def is_round_bracket(c):
     return c in BRACKETS
 
 def is_name(c):
-    return (c >= 'A' and c <= 'Z') or (c >= 'a' and c <= 'z')
+    return (c >= 'A' and c <= 'Z') or (c >= 'a' and c <= 'z') or c == AT or c == DOLLAR
 
 def is_symbol(c):
     return c in SYMBOLS
@@ -36,15 +51,6 @@ class Lexer:
         self.str = str
         self.offset = 0
         self.curr_token = None
-
-    @property
-    def curr_token(self):
-        return self._curr_token
-
-    @curr_token.setter
-    def curr_token(self, value):
-        print("set:", value)
-        self._curr_token = value
 
     def done(self):
         return self.offset == len(self.str)
@@ -75,7 +81,15 @@ class Lexer:
             return self.curr_token
 
         if is_name(c):
-            while self.offset < len(self.str) and is_name(self.str[self.offset]):
+            while self.offset < len(self.str) and (is_name(self.str[self.offset]) or is_digit(self.str[self.offset])):
+                token.append(self.str[self.offset])
+                self.offset += 1
+
+            self.curr_token = "".join(token)
+            return self.curr_token
+
+        if is_comparison_sym(c):
+            while self.offset < len(self.str) and is_comparison_sym(self.str[self.offset]):
                 token.append(self.str[self.offset])
                 self.offset += 1
 
@@ -91,69 +105,99 @@ class Lexer:
             res.append(self.next_token())
 
         return res
+class ParseError(Exception):
+    pass
 
-class NumberNode:
+class BaseNode:
+    def has_aggregation(self):
+        return False
+
+    def evaluate_scalar(self, rows):
+        res = self.evaluate(rows)
+
+        if self.has_aggregation():
+            return res[0]
+        else:
+            return res
+
+class NumberNode(BaseNode):
     def __init__(self, number):
         self.number = number
 
-    def evaluate(self, _variables):
-        return self.number
+    def evaluate(self, rows):
+        return [self.number] * len(rows)
 
     def __str__(self):
         return str(self.number)
 
-class VariableNode:
+class VariableNode(BaseNode):
     def __init__(self, name):
         self.name = name
 
-    def evaluate(self, variables):
-        if self.name in variables:
-            return variables[self.name]
-        else:
-            raise ParseError("unknown variable: " + self.name)
+    def evaluate(self, rows):
+        res = []
+
+        for index, variables in enumerate(rows):
+            if self.name in variables:
+                res.append(variables[self.name])
+            else:
+                row = json.dumps(variables)
+                raise ParseError(f"unknown variable: '{self.name}' in row #{index} '{row}'")
+        return res
 
     def __str__(self):
         return self.name
 
-class OperationNode:
+class OperationNode(BaseNode):
     def __init__(self, operator, left, right=None):
         self.operator = operator
         self.left = left
         self.right = right
 
-    def evaluate(self, variables):
-        op1 = self.left.evaluate(variables)
-        op2 = self.right.evaluate(variables)
+    def evaluate(self, rows):
+        op1 = self.left.evaluate(rows)
+        op2 = self.right.evaluate(rows)
 
         if self.operator == PLUS:
-            return op1 + op2
+            return [a + b for a, b in zip(op1, op2)]
 
         if self.operator == MINUS:
-            return op1 - op2
+            return [a - b for a, b in zip(op1, op2)]
 
         if self.operator == MULTIPLICATION:
-            return op1 * op2
+            return [a * b for a, b in zip(op1, op2)]
 
         if self.operator == DIVISION:
-            return op1 / op2
+            return [a / b for a, b in zip(op1, op2)]
+
+    def has_aggregation(self):
+        return self.left.has_aggregation() or self.right.has_aggregation()
 
     def __str__(self):
-        return str(self.left) + " " + str(self.operator) + " " + str(self.right)
+        return "(" + str(self.left) + " " + str(self.operator) + " " + str(self.right) + ")"
 
-class FuncNode:
-    def __init__(self, arg_nodes, func):
+class FuncNode(BaseNode):
+    def __init__(self, arg_nodes, func, agg=False):
         self.arg_nodes = arg_nodes
         self.func = func
+        self.agg = agg
 
-    def evaluate(self, variables):
-        args = list(map(lambda node: node.evaluate(variables), self.arg_nodes))
-        return self.func(*args)
+    def evaluate(self, rows):
+        # breakpoint()
+        # args_list = list(map(lambda node: node.evaluate(rows), self.arg_nodes))
+        # args_list = [node.evaluate(rows) for node in self.arg_nodes]
+        args_list = [list(map(lambda node: node.evaluate([row])[0], self.arg_nodes)) for row in rows]
+
+        if self.agg:
+            return [self.func(args_list)] * len(rows)
+        else:
+            return [self.func(*args) for args in args_list]
+
+    def has_aggregation(self):
+        return self.agg or any(map(lambda n: n.has_aggregation(), self.arg_nodes))
 
     def __str__(self):
         return str(self.func.__name__) + "(" + ", ".join(map(str, self.arg_nodes)) + ")"
-
-class ParseError(Exception):
-    pass
 
 class Parser:
     def __init__(self, str, const_values=None, func_values=None):
@@ -162,7 +206,23 @@ class Parser:
         self.func_values = func_values or {}
 
     def parse(self):
-        return self.parse_sum()
+        return self.parse_comparison()
+
+    def parse_comparison(self):
+        left = self.parse_sum()
+        node = None
+
+        while True:
+            token = self.lexer.curr_token
+
+            if token in COMPARISON_OPS:
+                if node:
+                    left = node
+
+                func = self.func_values[token]
+                node = FuncNode(func=func, arg_nodes=[left, self.parse_sum()])
+            else:
+                return node or left
 
     def parse_sum(self):
         left = self.parse_product()
@@ -191,7 +251,7 @@ class Parser:
         while True:
             token = self.lexer.curr_token
 
-            if token == MULTIPLICATION:
+            if token == MULTIPLICATION or token == DIVISION:
                 if node:
                     node = OperationNode(operator=token[0], left=node)
                 else:
@@ -210,41 +270,42 @@ class Parser:
             raise ParseError("invalid token: " + token)
 
         if is_digit(token[0]):
-            number = int(token)
-            node = NumberNode(number=number)
+            self.lexer.next_token()
+            return NumberNode(number=int(token))
 
-            token = self.lexer.next_token()
-            # if len(token) == 0:
-            #     raise ParseError("invalid token: " + token)
-
-            return node
+        if len(token) > 1 and token[0] == DOLLAR and is_digit(token[1]):
+            self.lexer.next_token()
+            return NumberNode(number=int(token[1:]))
 
         if is_name(token[0]):
+            func_token = token
             func = self.func_values.get(token)
 
             if func:
                 token = self.lexer.next_token()
-                if token != OPENING_BRACKET:
-                    raise ParseError("( is expected, got:" + token)
+                if token == OPENING_BRACKET:
+                    arg_nodes = [self.parse_comparison()]
 
-                arg_nodes = [self.parse_sum()]
+                    while self.lexer.curr_token == COMMA:
+                        arg_nodes.append(self.parse_comparison())
 
-                while self.lexer.curr_token == COMMA:
-                    arg_nodes.append(self.parse_sum())
+                    node = FuncNode(arg_nodes=arg_nodes, func=func, agg=func_token[0] == AT)
 
-                node = FuncNode(arg_nodes=arg_nodes, func=func)
+                    if self.lexer.curr_token != CLOSING_BRACKET:
+                        raise ParseError(") is expected, got:" + token)
+                    else:
+                        self.lexer.next_token()
+                        if len(token) == 0:
+                            raise ParseError("invalid token: " + token)
 
-                # breakpoint()
-                if self.lexer.curr_token != CLOSING_BRACKET:
-                    raise ParseError(") is expected, got:" + token)
-                else:
-                    self.lexer.next_token()
-                    if len(token) == 0:
-                        raise ParseError("invalid token: " + token)
+                        return node
 
                     return node
-
-                return node
+                elif func_token[0] == AT:
+                    # Allow agg func without brackets (empty parameters)
+                    return FuncNode(arg_nodes=[], func=func, agg=True)
+                else:
+                    raise ParseError("( is expected, got:" + token)
             else:
                 value = self.const_values.get(token)
 
@@ -256,7 +317,7 @@ class Parser:
                     return VariableNode(name=token)
 
         if token == OPENING_BRACKET:
-            node = self.parse_sum()
+            node = self.parse_comparison()
             token = self.lexer.curr_token
 
             if token != CLOSING_BRACKET:
@@ -268,4 +329,95 @@ class Parser:
 
                 return node
 
-        raise ParseError("term is expected")
+        raise ParseError("term is expected, got: " + token)
+
+class RoiCalculator:
+    @staticmethod
+    def lt(a, b):
+        return a < b
+
+    @staticmethod
+    def eq(a, b):
+        return a == b
+
+    @staticmethod
+    def gt(a, b):
+        return a > b
+
+    @staticmethod
+    def sum(value_and_predicate):
+        res = 0
+
+        for item in value_and_predicate:
+            predicate = True
+            value = item[0]
+
+            if len(item) == 2:
+                value, predicate = item
+
+            if predicate:
+                res += value
+
+        return res
+
+    @staticmethod
+    def count(value_and_predicate):
+        res = 0
+
+        for item in value_and_predicate:
+            predicate = True
+
+            if len(item) == 2:
+                _, predicate = item
+
+            if predicate:
+                res += 1
+
+        return res
+
+    FUNC_VALUES = {
+        "min": min,
+        "max": max,
+        "<": lt.__get__(object),
+        "=": eq.__get__(object),
+        ">": gt.__get__(object),
+        "@sum": sum.__get__(object),
+        "@count": count.__get__(object),
+    }
+
+    CONST_VALUES = {
+        "True": True,
+        "False": False,
+    }
+
+    def __init__(self, revenue=None, investment=None, filter=None):
+        self.revenue = revenue
+        self.investment = investment
+        self.filter = filter
+
+        self.revenue_ast = self.build_ast(self.revenue)
+        self.investment_ast = self.build_ast(self.investment)
+        self.filter_ast = self.build_ast(self.filter)
+
+    def build_ast(self, expression):
+        if expression:
+            parser = Parser(expression, const_values=self.CONST_VALUES, func_values=self.FUNC_VALUES)
+            return parser.parse()
+
+    def calculate(self, rows):
+        filtered_rows = rows
+
+        if self.filter_ast:
+            filtered_rows = [row for row, marked in zip(rows, self.filter_ast.evaluate(rows)) if marked]
+
+        revenue = self.revenue_ast.evaluate_scalar(filtered_rows)
+        investment = self.investment_ast.evaluate_scalar(filtered_rows)
+        roi = (revenue - investment) / investment
+
+        return {
+            "count": len(filtered_rows),
+            "filtered_rows": filtered_rows,
+            "revenue": revenue,
+            "investment": investment,
+            "roi": roi,
+        }
--- tests/api/utils/test_roi_calc.py
+++ tests/api/utils/test_roi_calc.py
@@ -1,25 +1,51 @@
-from a2ml.api.utils.roi_calc import Lexer, Parser
-
+import pytest
 import unittest
+
+from a2ml.api.utils.roi_calc import Lexer, Parser, RoiCalculator
+
 
 class TestLexer(unittest.TestCase):
     def test_bike_rental_investment(self):
-        lexer = Lexer("2*max(P-10,0)")
+        lexer = Lexer("$2*max(P-10,0)")
         res = lexer.all_tokens()
 
-        assert ['2', '*', 'max', '(', 'P', '-', '10', ',', '0', ')'] == res
+        assert ['$2', '*', 'max', '(', 'P', '-', '10', ',', '0', ')'] == res
 
     def test_bike_rental_investment_with_spaces(self):
-        lexer = Lexer("2 * max(P - 10, 0)")
+        lexer = Lexer("$2 * max(P - 10, 0)")
         res = lexer.all_tokens()
 
-        assert ['2', '*', 'max', '(', 'P', '-', '10', ',', '0', ')'] == res
+        assert ['$2', '*', 'max', '(', 'P', '-', '10', ',', '0', ')'] == res
 
     def test_options_app_revenue(self):
-        lexer = Lexer("@sum((1 + A) * 100)")
+        lexer = Lexer("@sum((1 + A) * $100)")
         res = lexer.all_tokens()
 
-        assert ['@', 'sum', '(', '(', '1', '+', 'A', ')', '*', '100', ')'] == res
+        assert ['@sum', '(', '(', '1', '+', 'A', ')', '*', '$100', ')'] == res
+
+    def test_some_filter(self):
+        lexer = Lexer("A > P and B1 == B2 and C1 != C2 and D1 >= D2")
+        res = lexer.all_tokens()
+
+        assert ['A', '>', 'P', 'and', 'B1', '==', 'B2', 'and', 'C1', '!=', 'C2', 'and', 'D1', '>=', 'D2'] == res
+
+@pytest.mark.parametrize(
+    "expression, result, exected_parsed_expression",
+    [
+        pytest.param("1 + (2*2 - 3)", 2, "(1 + ((2 * 2) - 3))"),
+        pytest.param("1 - (2*2 - 3)", 0, "(1 - ((2 * 2) - 3))"),
+        pytest.param("1 - 2*10 - 1", -20, "((1 - (2 * 10)) - 1)"),
+        pytest.param("(2 + 2) * 2", 8, "((2 + 2) * 2)"),
+        pytest.param("2 + 2 * 2", 6, "(2 + (2 * 2))"),
+        pytest.param("2 + 2 / 2", 3, "(2 + (2 / 2))"),
+    ]
+)
+def test_parser_with_arithmetic(expression, result, exected_parsed_expression):
+    tree = Parser(expression).parse()
+
+    assert exected_parsed_expression == str(tree)
+    assert [result] == tree.evaluate([{}])
+    assert [result, result] == tree.evaluate([{}, {}])
 
 class TestParser(unittest.TestCase):
     def test_bike_rental_investment(self):
@@ -29,25 +55,61 @@ class TestParser(unittest.TestCase):
         parser = Parser(expression, func_values=func_values)
         tree = parser.parse()
 
-        assert expression == str(tree)
-        assert 20 == tree.evaluate(variables)
+        assert "(2 * max((P - 10), 0))" == str(tree)
+        assert False == tree.has_aggregation()
+        assert [20] == tree.evaluate([variables])
 
     def test_bike_rental_revenue(self):
-        expression = "min(A, P) * 10"
+        expression = "min(A, P) * $10"
         variables = { "P": 20, "A": 10 }
         func_values = { "min": min }
         parser = Parser(expression, func_values=func_values)
         tree = parser.parse()
 
-        assert expression == str(tree)
-        assert 100 == tree.evaluate(variables)
+        assert "(min(A, P) * 10)" == str(tree)
+        assert False == tree.has_aggregation()
+        assert [100] == tree.evaluate([variables])
 
+    def test_options_app_revenue(self):
+        expression = "@sum((1 + A) * $100)"
 
-    # def test_credit_analysis_revenue():
-    #     expression = "@sum(1050, A == True)"
-    #     const_values = { "P": True, "A": True }
-    #     func_values = { "max": max }
-    #     parser = Parser(expression, const_values=const_values, func_values=func_values)
-    #     tree = parser.parse()
+        variables_list = [
+            { "P": 0.50, "A": 0.6 },
+            { "P": 0.45, "A": 0.7 },
+            { "P": 0.45, "A": 0.75 },
+        ]
 
-    #     assert 20 == tree.evaluate()
+        func_values = { "@sum": RoiCalculator.sum }
+        parser = Parser(expression, func_values=func_values)
+        tree = parser.parse()
+
+        assert "sum(((1 + A) * 100))" == str(tree)
+        assert True == tree.has_aggregation()
+        assert (0.6 + 0.7 + 0.75 + 3) * 100 == tree.evaluate_scalar(variables_list)
+
+class TestRoiCalculator:
+    def test_credit_analysis(self):
+        calc = RoiCalculator(
+            filter="P=True",
+            revenue="@sum($1050, A=True)",
+            investment="@count * $1000",
+        )
+
+        res = calc.calculate(
+            [
+                {"A": True, "P": True},
+                {"A": True, "P": False},
+                {"A": False, "P": True},
+                {"A": False, "P": False},
+            ]
+        )
+
+        assert 2 == res["count"]
+        assert res["filtered_rows"] == [
+            {"A": True, "P": True},
+            {"A": False, "P": True},
+        ]
+
+        assert 1050 == res["revenue"]
+        assert 2000 == res["investment"]
+        assert -0.475 == res["roi"]
