1a3547d2629cdd376016cfc1766c33f2216c1caf
skatedplrn <evgeny@dplrn.com>
2021-04-09 11:37:11 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ install_requires = [
     'scipy==1.5.2',
     'asyncio',
     'boto3',
-    'auger-hub-api-client==0.7.3',
+    'auger-hub-api-client==0.7.4',
     'click',
     'shortuuid',
     'docutils<0.16,>=0.10',
