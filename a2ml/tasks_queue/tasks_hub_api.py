02259759189fe379df4cb3bb985ca247fe705a4f
Alex Emelyanov <holyketzer@gmail.com>
2021-04-12 21:10:51 +0300

    Support additional features in validate_roi_syntax task (fix)

--- a2ml/tasks_queue/tasks_hub_api.py
+++ a2ml/tasks_queue/tasks_hub_api.py
@@ -614,7 +614,7 @@ def build_review_data_task(params):
         date_col = ctx.config.get_list('experiment/date_time')[0]
 
     retrain_policy = None
-    if ctx.config.get('experiment/retrain_policy_type'):    
+    if ctx.config.get('experiment/retrain_policy_type'):
         retrain_policy = {'type': ctx.config.get('experiment/retrain_policy_type'), 'value':ctx.config.get('experiment/retrain_policy_value')}
 
     return ModelReview(params).build_review_data(
@@ -694,4 +694,5 @@ def presign_s3_url_task(params):
 def validate_roi_syntax_task(params):
     return ModelReview(params).validate_roi_syntax(
         expressions=params["expressions"],
+        features=params.get("features", []),
     )
